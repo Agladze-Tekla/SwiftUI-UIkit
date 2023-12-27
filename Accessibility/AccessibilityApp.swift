8a965b2315057783ad311787cf043eb4735c6f58
Tekla <tekla@Teklas-Mac.local>
2023-12-27 09:53:44 -0800

    first commit

---
+++ Accessibility/AccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityApp.swift
+//  Accessibility
+//
+//  Created by Tekla on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NewsPageView()
+        }
+    }
+}
